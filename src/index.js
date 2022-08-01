1a00c1439fc8adb6c19384cf7f6a87c6e5ce3799
Jo <joel.panvel@gmail.com>
2022-08-02 00:59:15 +0530

    tweets are successfully added

--- src/index.js
+++ src/index.js
@@ -211,6 +211,33 @@ if (currentRoute === routes.browseThread) {
 
       let tweetHtmlPage = document.querySelector('.tweet-content');
       tweetHtmlPage.innerHTML = tweetContent;
+
+      // var inlineCode = document.createTextNode('<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>');
+      // tweetHtmlPage.appendChild(inlineCode);
+      // document.body.appendChild(tweetHtmlPage);
+      const src = 'https://platform.twitter.com/widgets.js';
+
+      function loadJS(FILE_URL, async = true) {
+        let scriptEle = document.createElement("script");
+      
+        scriptEle.setAttribute("src", FILE_URL);
+        scriptEle.setAttribute("type", "text/javascript");
+        scriptEle.setAttribute("async", async);
+      
+        document.body.appendChild(scriptEle);
+      
+        // success event 
+        scriptEle.addEventListener("load", () => {
+          console.log("File loaded")
+        });
+         // error event
+        scriptEle.addEventListener("error", (ev) => {
+          console.log("Error on loading file", ev);
+        });
+
+      }
+
+      loadJS(src, true);
     })
 
     console.log("hello")
